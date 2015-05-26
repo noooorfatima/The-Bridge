7167dd5b7ff2458483e8b3a2e75294a1cc37cb84
root <root@ds-web.haverford.edu>
2015-05-26 17:15:02 -0400

    models.py changed to reflect database update.

--- new_bridge/settings.py
+++ new_bridge/settings.py
@@ -61,10 +61,23 @@ WSGI_APPLICATION = 'new_bridge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bridge',
+        'USER': 'bridge',
+        'PASSWORD': 'Br1dg3',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
