d296f0755290755704a0c91397886342df9e8fd7
Jack Rasiel <jrasiel@haverford.edu>
2015-08-26 17:02:20 -0400

    Added import script and models for new DB structure.

--- new_bridge/models.py
+++ new_bridge/models.py
@@ -10,87 +10,136 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from treebeard.mp_tree import MP_Node
+
+class TextStructureNode(MP_Node):
+    text_name = models.CharField(max_length=52, blank=True)
+    subsection_level = models.SmallIntegerField(blank=False)
+    subsection_number = models.IntegerField(blank=False)
+    least_mindiv = models.IntegerField(blank=False)
+
+    node_order_by = ['subsection_number']
+    def __unicode__(self):
+        #Climb tree to find parent location information:
+        loc_string = ""
+        parent = self      
+        while not parent.is_root():
+            loc_string =unicode(parent.subsection_number) + '.' + loc_string
+            parent = parent.get_parent()
+        #Descend tree to find child location information:
+        child = self
+        while not child.is_leaf():
+            child = child.get_last_child()
+            loc_string += '.'+'x'
+        return self.text_name + '\t' + loc_string
+
+class TextStructureGlossary(models.Model):
+    text_name = models.CharField(max_length=52, blank=False)
+    subsection_level = models.SmallIntegerField(blank=False)
+    subsection_name = models.CharField(max_length=20, blank=True)
+    def __unicode__(self):
+        s = 'TEXT:\t'+self.text_name
+        s += '\nSUBSECTION LVL:\t'+self.subsection_level
+        s += '\nSUBSECTION NAME:\t'+self.subsection_name
+        return s
+
+class WordAppearences(models.Model):
+    text_name = models.CharField(max_length=52, blank=False)
+    word_id = models.IntegerField(blank=False)
+    mindiv = models.SmallIntegerField(blank=False)
+    def __unicode__(self):
+        s = 'TEXT:\t'+self.text_name
+        s += '\nWORD ID:\t'+self.word_id
+        s += '\nLOCATION:\t'+self.mindiv
+        return s
+
+#class TextMetadata(models.Model):
+#   human-readable text name
+#   other meta-data?
+#class WordProperties(models.Model):
 
 
 class BookTable(models.Model):
     id = models.IntegerField(primary_key=True)
-    title = models.CharField(db_column='Title', max_length=30, blank=True) # Field name made lowercase.
-    appearences = models.CharField(db_column='Appearences', max_length=17360, blank=True) # Field name made lowercase.
-    field_book_text = models.CharField(db_column=' Book/Text', max_length=52, blank=True) # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it started with '_'.
+    title = models.CharField(db_column='Title', max_length=30, blank=True) 
+    appearences = models.CharField(db_column='Appearences', max_length=17360, blank=True) 
+    field_book_text = models.CharField(db_column='BookText', max_length=52, blank=True) 
+    def __unicode__(self):
+        return self.title
     class Meta:
-        managed = False
+        managed = True
         db_table = 'book_table'
 
-
 class WordTable(models.Model):
-    id = models.IntegerField(primary_key=True)
-    title = models.CharField(db_column='TITLE', max_length=17, blank=True) 
-    display_lemma = models.CharField(db_column='DISPLAY LEMMA', max_length=84, blank=True) 
-    #display_lemma_macronless = models.CharField(db_column='DISPLAY LEMMA MACRONLESS', max_length=78, blank=True) # NOT IN DEV DB (db.sqlite)
-    #display_lemma_macron_field = models.CharField(db_column='DISPLAY LEMMA MACRON', max_length=84, blank=True) # NOT IN DEV DB (db.sqlite)
-    english_core = models.CharField(db_column='ENGLISH-CORE', max_length=126, blank=True) 
-    english_extended = models.CharField(db_column='ENGLISH-EXTENDED', max_length=247, blank=True) 
-    #aeneid_definition = models.CharField(db_column='AENEID-DEFINITION', max_length=696, blank=True) # NOT IN DEV DB (db.sqlite)
-    #catullus_definition = models.CharField(db_column='CATULLUS-DEFINITION', max_length=154, blank=True) # NOT IN DEV DB (db.sqlite)
-    lasla_morph_1 = models.CharField(db_column='LASLA MORPH 1', max_length=43, blank=True) 
-    lasla_morph_2 = models.CharField(db_column='LASLA MORPH 2', max_length=42, blank=True) 
-    lasla_combined = models.CharField(db_column='LASLA Combined', max_length=3, blank=True) 
-    decl = models.CharField(db_column='Decl', max_length=1, blank=True) 
-    conj = models.CharField(db_column='Conj', max_length=2, blank=True) 
-    idiom = models.CharField(db_column='Idiom', max_length=1, blank=True) 
-    reg_adj_adv = models.CharField(db_column='Reg Adj/Adv', max_length=2, blank=True) 
-    number = models.CharField(db_column='Number', max_length=1, blank=True) 
-    proper = models.CharField(db_column='Proper', max_length=1, blank=True) 
-    part_of_speech = models.CharField(db_column='Part Of Speech', max_length=12, blank=True) 
-    #exclude_1_0 = models.CharField(db_column='Exclude 1/0', max_length=1, blank=True) # NOT IN DEV DB (db.sqlite)
-    #notes = models.CharField(db_column='NOTES', max_length=123, blank=True) # NOT IN DEV DB (db.sqlite)
-    dcc_frequency_rank = models.CharField(db_column='DCC FREQUENCY RANK', max_length=12, blank=True) 
-    dcc_frequency_group = models.CharField(db_column='DCC FREQUENCY GROUP', max_length=4, blank=True) 
-    dcc_semantic_group = models.CharField(db_column='DCC SEMANTIC GROUP', max_length=34, blank=True) 
+    title = models.CharField(db_column='title', max_length=30, blank=True) 
+    display_lemma = models.CharField(db_column='display_lemma', max_length=84, blank=True) 
+    display_lemma_macronless = models.CharField(db_column='display_lemma_macronless', max_length=83, blank=True) 
+    english_core = models.CharField(db_column='english_core', max_length=155, blank=True) 
+    english_extended = models.CharField(db_column='english_extended', max_length=247, blank=True) 
+    lnm_definition = models.CharField(db_column='lnm_definition', max_length=74, blank=True) 
+    aeneid_definition = models.CharField(db_column='aeneid_definition', max_length=1168, blank=True) 
+    catullus_definition = models.CharField(db_column='catullus_definition', max_length=245, blank=True) 
+    decl = models.CharField(db_column='decl', max_length=1, blank=True) 
+    conj = models.CharField(db_column='conj', max_length=1, blank=True) 
+    reg_adj_adv = models.CharField(db_column='reg_adj_adv', max_length=1, blank=True) 
+    proper = models.CharField(db_column='proper', max_length=1, blank=True) 
+    part_of_speech = models.CharField(db_column='part_of_speech', max_length=24, blank=True) 
+    dcc_frequency_rank = models.CharField(db_column='dcc_frequency_rank', max_length=5, blank=True) 
+    dcc_frequency_group = models.CharField(db_column='dcc_frenquency_group', max_length=2, blank=True) 
+    dcc_semantic_group = models.CharField(db_column='dcc_semantic_group', max_length=34, blank=True) 
+    def __unicode__(self):
+        return self.title
     class Meta:
-        managed = False
+        managed = True
         db_table = 'word_table'
 
 
 class BookTitles(models.Model):
-    title_of_book = models.TextField(db_column='Title of Book') # Field name made lowercase. Field renamed to remove unsuitable characters.
+    title_of_book = models.TextField(db_column='Title of Book') 
+    def __unicode__(self):
+        return self.title_of_book
     class Meta:
         managed = True
         db_table = 'book_titles'
 
-    
 class BookTableGreek(models.Model):
     id = models.IntegerField(primary_key=True)
-    title = models.CharField(db_column='Title', max_length=43, blank=True) # Field name made lowercase.
-    appearences = models.CharField(db_column='Appearences', max_length=8, blank=True) # Field name made lowercase.
-    field_book_text = models.CharField(db_column=' Book/Text', max_length=44, blank=True) # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it started with '_'.
+    title = models.CharField(db_column='Title', max_length=43, blank=True) 
+    appearences = models.CharField(db_column='Appearences', max_length=8, blank=True) 
+    field_book_text = models.CharField(db_column=' Book/Text', max_length=44, blank=True) 
+    def __unicode__(self):
+        return self.title
     class Meta:
         managed = True
         db_table = 'book_table_greek'
 
 class WordTableGreek(models.Model):
-    title = models.CharField(db_column='TITLE', max_length=43, blank=True, null=True) # Field name made lowercase.
-    accented_lemma = models.CharField(db_column='accented lemma', max_length=50, blank=True, null=True) # Field renamed to remove unsuitable characters.
-    search_lemma = models.CharField(db_column='SEARCH LEMMA', max_length=43, blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    display_lemma = models.CharField(db_column='DISPLAY LEMMA', max_length=175, blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    english_definition = models.CharField(db_column='English Definition', max_length=135, blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    questions = models.IntegerField(db_column='Questions', blank=True, null=True) # Field name made lowercase.
-    decl = models.CharField(db_column='Decl', max_length=4, blank=True, null=True) # Field name made lowercase.
-    idiom = models.IntegerField(db_column='Idiom', blank=True, null=True) # Field name made lowercase.
-    reg_adject_adv_form = models.IntegerField(db_column='Reg Adject/Adv/Form', blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    proper = models.IntegerField(db_column='Proper', blank=True, null=True) # Field name made lowercase.
-    part_of_speech = models.CharField(db_column='Part Of Speech', max_length=24, blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    exclude_1_0 = models.IntegerField(db_column='Exclude 1/0', blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    notes = models.CharField(db_column='Notes', max_length=34, blank=True, null=True) # Field name made lowercase.
-    dcc_semantic_group = models.CharField(db_column='DCC SEMANTIC GROUP', max_length=34, blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    dcc_core_frequency = models.CharField(db_column='DCC Core Frequency', max_length=4, blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
-    herodotus_1_frequency_rank = models.IntegerField(db_column='Herodotus 1 frequency rank', blank=True, null=True) # Field name made lowercase. Field renamed to remove unsuitable characters.
+    title = models.CharField(db_column='TITLE', max_length=43, blank=True, null=True) 
+    accented_lemma = models.CharField(db_column='accented lemma', max_length=50, blank=True, null=True) 
+    search_lemma = models.CharField(db_column='SEARCH LEMMA', max_length=43, blank=True, null=True) 
+    display_lemma = models.CharField(db_column='DISPLAY LEMMA', max_length=175, blank=True, null=True) 
+    english_definition = models.CharField(db_column='English Definition', max_length=135, blank=True, null=True) 
+    questions = models.IntegerField(db_column='Questions', blank=True, null=True) 
+    decl = models.CharField(db_column='Decl', max_length=4, blank=True, null=True) 
+    idiom = models.IntegerField(db_column='Idiom', blank=True, null=True) 
+    reg_adject_adv_form = models.IntegerField(db_column='Reg Adject/Adv/Form', blank=True, null=True) 
+    proper = models.IntegerField(db_column='Proper', blank=True, null=True) 
+    part_of_speech = models.CharField(db_column='Part Of Speech', max_length=24, blank=True, null=True) 
+    exclude_1_0 = models.IntegerField(db_column='Exclude 1/0', blank=True, null=True) 
+    notes = models.CharField(db_column='Notes', max_length=34, blank=True, null=True) 
+    dcc_semantic_group = models.CharField(db_column='DCC SEMANTIC GROUP', max_length=34, blank=True, null=True) 
+    dcc_core_frequency = models.CharField(db_column='DCC Core Frequency', max_length=4, blank=True, null=True) 
+    herodotus_1_frequency_rank = models.IntegerField(db_column='Herodotus 1 frequency rank', blank=True, null=True) 
+    def __unicode__(self):
+        return self.title
     class Meta:
         managed = True
         db_table = 'word_table_greek'
 
 class BookTitlesGreek(models.Model):
-    title_of_book = models.TextField(db_column='Title of Book') # Field name made lowercase. Field renamed to remove unsuitable characters.
+    title_of_book = models.TextField(db_column='Title of Book') 
+    def __unicode__(self):
+        return self.title_of_book
     class Meta:
         managed = True
         db_table = 'book_titles_greek'
