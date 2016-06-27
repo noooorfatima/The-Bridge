927fcda28fac2d74973fb520982de3f720403b19
Blair <btrush@haverford.edu>
2016-06-27 11:57:55 -0400

    changed db stuff in settings

--- new_bridge/settings.py
+++ new_bridge/settings.py
@@ -63,11 +63,10 @@ DJANGO_SETTINGS_MODULE = ( os.path.join(PROJECT_ROOT, 'settings')
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3', #'django.db.backends.postgresql_psycopg2',  Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'bridge',                      # Or path to database file if using sqlite3.
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             #'USER': 'bridgeuser',
             #'PASSWORD': 'bridge',
-            'HOST': '165.82.13.100',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             #'PORT': '',                      # Set to empty string for default.
         }
     }
