e7225def7aa180ffba0a6ab285143ada651c4bc7
Wreck Room Computer <digitalscholarship@wreckroom>
2016-06-27 10:45:31 -0400

    missing comma in settins on db info

--- new_bridge/settings.py
+++ new_bridge/settings.py
@@ -63,7 +63,7 @@ DJANGO_SETTINGS_MODULE = ( os.path.join(PROJECT_ROOT, 'settings')
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3', #'django.db.backends.postgresql_psycopg2',  Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'bridge'                      # Or path to database file if using sqlite3.
+            'NAME': 'bridge',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             #'USER': 'bridgeuser',
             #'PASSWORD': 'bridge',
