780bf7d53ea4bd603a0bdccac74d119630ab8cc1
Noor Fatima <noooorfatima98@gmail.com>
2019-05-29 18:18:42 +0000

    testing

--- lemmatizer/convert_lemmata_format.py
+++ lemmatizer/convert_lemmata_format.py
@@ -256,7 +256,8 @@ if __name__ == '__main__':
                                   args.output_format, include_ambiguous = False,
                                   lemmata_column = args.lemmata_column)
 
-        print(("Saving changes to {}".format(args.file_path)))
-        print("Finished.")
+       # print(("Saving changes to {}".format(args.file_path)))
+      
         workbook.save(args.file_path)
         print("Finished.")
+        print(("Saving changes to {}".format(args.file_path)))
