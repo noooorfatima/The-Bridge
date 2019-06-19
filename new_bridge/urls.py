ac7b25b65c97f69d5e5aa2460673df277169503b
noooorfatima <50842914+noooorfatima@users.noreply.github.com>
2019-06-19 15:32:35 -0400

    Update urls.py

--- new_bridge/urls.py
+++ new_bridge/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path('get_words/<language>/<text>/<bookslist>/<text_from>/<text_to>/<add_remove>/', views.get_words),
     path('about', views.AboutView),
     path('oracle/', views.Oracle),
-    path('tools/', lemviews.lemmatizer, name='tools'),
-    path('tools/Lemmatizer', lemviews.lemmatizer, name='lemmatized'),
+    path('lemmatizer/', lemviews.lemmatizer, name='lemmatizer'),
+    path('lemmatizer/Lemmatizer', lemviews.lemmatizer, name='lemmatized'),
     path('format/', lemviews.formatlemmatizedtext, name='format'),
     path('formatted/', lemviews.formatlemmatizedtext, name='formatted'),
 ]
