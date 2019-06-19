12b802cdd1bfec319df6493716ec9c24b394afd5
noooorfatima <50842914+noooorfatima@users.noreply.github.com>
2019-06-19 13:37:11 -0400

    Update views.py

--- lemmatizer/views.py
+++ lemmatizer/views.py
@@ -110,8 +110,8 @@ def lemmatizer(request):
                     print('entered the if statement for excel')
                 # Here we send the output file to lemmatized.html (tmpEDoVlX_Input.xlsx)
                     if lem_format == 'bridge':
-                        output_file = str(f.name).split('.')[0] + '.xlsx'
-                        output_file = output_file.split('/')[2]
+                        output_file = str(newname).split('.')[0] + '.xlsx'
+                        #output_file = output_file.split('/')[2]
                         print('created output_file: added .xlsx to a temporary file if lemformat is bridge')
 
 
